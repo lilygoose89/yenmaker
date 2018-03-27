f94a4a2b572232cc9860a62bfd46ff242f44169e
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2018-03-27 23:00:15 +0700

    set up sidebar nav
    rachel set up js

--- _site/js/main.js
+++ _site/js/main.js
@@ -6,6 +6,26 @@ $(document).ready(function(){
 
 });
 
-$('#nav-icon3').click(function(){
-  $(this).toggleClass('open');
+// $('#nav-icon3').click(function(){
+//   $(this).toggleClass('open');
+// });
+
+$("#nav-icon3").click(function(){
+  $(this).toggleClass("open");
+  $("#sidebar").toggleClass("open");
+  $("html").addClass("noscroll");
+  $("#overlay").toggle();
+
+  if($(this).hasClass("open")){
+    $(this).prependTo('#sidebar');
+  } else {
+    $(this).prependTo('#header .bg-grey');
+  }
+});
+
+$("#overlay").click(function(){
+  $("#nav-icon3").removeClass("open").prependTo('#header .bg-grey');
+  $("#sidebar").removeClass("open");
+  $("html").removeClass("noscroll");
+  $(this).hide();
 });

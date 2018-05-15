3505c0a1c890bb26a641604c26e3d73ad88d117b
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2018-05-15 19:18:28 +0700

    edits for beer
    - removed gold background
    - removed fixed nav background
    - changed pics around
    - adjusted hero padding
    - changed "shop now" button style
    - fixed #side-nav width for mobile
    - revised layout with _includes
    - FIXED missing </a> in nav!!!
    - moved main.js

--- _site/main.js
+++ _site/main.js
@@ -6,26 +6,20 @@ $(document).ready(function(){
 
 });
 
-// $('#nav-icon3').click(function(){
-//   $(this).toggleClass('open');
-// });
-
 $("#nav-icon3").click(function(){
   $(this).toggleClass("open");
   $("#sidebar").toggleClass("open");
-  $("html").addClass("noscroll");
   $("#overlay").toggle();
 
   if($(this).hasClass("open")){
     $(this).prependTo('#sidebar');
   } else {
-    $(this).prependTo('#header .bg-grey');
+    $(this).prependTo('#header .nav');
   }
 });
 
 $("#overlay").click(function(){
-  $("#nav-icon3").removeClass("open").prependTo('#header .bg-grey');
+  $("#nav-icon3").removeClass("open").prependTo('#header .nav');
   $("#sidebar").removeClass("open");
-  $("html").removeClass("noscroll");
   $(this).hide();
 });
